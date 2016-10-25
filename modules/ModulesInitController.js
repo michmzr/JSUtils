ede4f5206c80592d68668198f6d55308e8f021be
Michał Mazur <michal.mazur@truesoftware.pl>
2016-10-25 09:12:09 -0500

    - added simple jquery plugins initiation other than select2, datepicker

--- modules/ModulesInitController.js
+++ modules/ModulesInitController.js
@@ -4,78 +4,97 @@
  * General module controller for controlling javascript modules and plugins like DatePicker, Select2
  *
  * @type {{init}}
- * @version 1.0
+ * @version 1.1
  */
-var ModulesInitController = (function () {
+var ModulesInitController = (function ()
+{
 
-    var settings = {
-        "triggerElemType": ".modulesInit",
-        "dataAttributes": {
-            "moduleName": "init-module",
-            "config": "init-module-config"
-        },
-        "modules": {
-            "datePicker": DatePickerModule,
-            "select2": Select2Module
-        }
-    };
+	var settings = {
+		"triggerElemType": ".modulesInit",
+		"dataAttributes": {
+			"moduleName": "init-module",
+			"config": "init-module-config"
+		},
+		"modules": {
+			"datePicker": DatePickerModule,
+			"select2": Select2Module
+		}
+	};
 
-    var intiModuleInElement = function (elem) {
-            console.info("init module for element " + jqueryElemToString(elem));
+	var initModuleInElement = function (elem)
+		{
+			console.info("init module for element " + jqueryElemToString(elem));
 
-            var s = settings;
-            var elemModuleName = $(elem).data(s.dataAttributes.moduleName);
+			var s = settings;
+			var elemModuleName = $(elem).data(s.dataAttributes.moduleName);
 
-            if (elemModuleName) {
-                var module = s.modules[elemModuleName];
+			if (elemModuleName)
+			{
+				var module = s.modules[elemModuleName];
 
-                if (module != undefined) {
-                    var initConfigData = $(elem).data(s.dataAttributes.config);
+				var initConfigData = $(elem).data(s.dataAttributes.config);
 
-                    //parse module configuration
-                    var initConfig = {};
+				//parse module configuration
+				var initConfig = {};
 
-                    if (initConfigData) {
-                        if (initConfigData instanceof Object) {
-                            initConfig = initConfigData;
-                        } else {
-                            initConfig = (initConfigData.length > 0) ? $.parseJSON(initConfigData) : {}
-                        }
-                    }
+				if (initConfigData)
+				{
+					if (initConfigData instanceof Object)
+					{
+						initConfig = initConfigData;
+					} else
+					{
+						initConfig = (initConfigData.length > 0) ? $.parseJSON(initConfigData) : {}
+					}
+				}
 
-                    module.init(elem, initConfig);
-                } else {
-                    console.error("Module " + module + " is uknown or not available");
-                }
-            } else {
-                console.error("Html attribute '" + s.dataAttributes.moduleName + "' is required for element " + jqueryElemToString(elem));
-            }
-        },
+				if (module != undefined)
+				{
+					module.init(elem, initConfig);
+				} else
+				{
+					initPlugin(elem, elemModuleName , initConfig)
+				}
+			} else
+			{
+				console.error("Html attribute '" + s.dataAttributes.moduleName + "' is required for element " + jqueryElemToString(elem));
+			}
+		},
 
-        init = function (el) {
-            var s = settings;
+		initPlugin = function ($el, pluginName, options)
+		{
+			console.log("Plugin name " + pluginName + " with config " + options);
 
-	     //   console.info("Init modules for elements with class " + s.triggerElemType);
+			$($el)[pluginName](options);
+		},
 
-            if (el)
-            {
-	            // console.info("Init modules in el");
-            	// console.log($(el).find(s.triggerElemType));
+		init = function (el)
+		{
+			var s = settings;
 
-                $(el).find(s.triggerElemType).each(function (index, elem) {
-                    intiModuleInElement(elem);
-                });
-            }else
-            {
-                // console.info("Init modules outside el");
+			//   console.info("Init modules for elements with class " + s.triggerElemType);
 
-                $(s.triggerElemType).each(function (index, elem) {
-                    intiModuleInElement(elem);
-                });
-            }
-        };
+			if (el)
+			{
+				// console.info("Init modules in el");
+				// console.log($(el).find(s.triggerElemType));
 
-    return {
-        init: init
-    }
+				$(el).find(s.triggerElemType).each(function (index, elem)
+				{
+					initModuleInElement(elem);
+				});
+			} else
+			{
+				// console.info("Init modules outside el");
+
+				$(s.triggerElemType).each(function (index, elem)
+				{
+					initModuleInElement(elem);
+				});
+			}
+		};
+
+	return {
+		init: init
+	}
 })();
